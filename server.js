c989a69fe5904fa54ffc163512ee5081c0645507
JEREMY-JavaDev <simen@LAPTOP-V2QH3M92>
2024-02-07 17:19:08 +0100

    Your commit message here

---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require("express");
+const { disconnect } = require("process");
+const { listen } = require("socket.io");
+let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
+users = [];
+connections = [];
+server.listen(3000);
+app.get("/", function(req, resp) {
+    //route for localhost:3000/
+    resp.sendFile(__dirname + "/index.html");
+});
+
+io.sockets.on("connection", function(socket) {
+    //when client connects to server*/$$
+    connections.push(socket); //add plug details to custom array
+    console.log("connected : %s socket connected", connections.length); //curr connections
+    socket.on("disconnect", function(data){
+        //client disconnects from server
+        connections.splice(connections.indexof(socket), 1); //delete plug details
+        console.log("Dsiconnected : %s socket connected", connections.length); //curr connections
+
+    });
+
+    socket.on("send message", function(data) {
+        console.log(data);
+        io.sockets.emit("new message", {msg: data});
+    });
+});
+
+console.log("server is listening");
